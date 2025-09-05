fb84ebf53f8b01090770c11f7d193286362b675f
nqluong <nguyenluong052004@gmail.com>
2025-09-05 10:33:17 +0700

    init project

---
+++ src/main/java/org/project/appointment_project/AppointmentProjectApplication.java
@@ -0,0 +1,13 @@
+package org.project.appointment_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/project/appointment_project/AppointmentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.appointment_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
